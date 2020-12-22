9f4530859d466dd5c4b4a129eefa267a329dae12
Igor Fraga <igorf.ext@vr.com.br>
2020-12-21 22:09:42 -0300

    Exercices from Sector 3 - TDD by Example until Chapter 22, Apples and Oranges

---
+++ src/main/java/guru/springframework/Money.java
@@ -0,0 +1,21 @@
+package guru.springframework;
+
+public class Money {
+
+    protected int amount;
+//
+//    public Money(int amount) {
+//        this.amount = amount;
+//    }
+//
+//    Money times(int multiplier) {
+//        return new Money(amount * multiplier);
+//    }
+//
+    public boolean equals(Object object) {
+        Money money = (Money) object;
+        return this.amount == money.amount
+                && this.getClass().equals(object.getClass()); //compares also the type of the Currency
+    }
+
+}
---
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -0,0 +1,43 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+public class MoneyTest {
+    @Test
+    void testMultiplicationDollar() {
+        Dollar five = new Dollar(5);
+
+        Dollar product = five.times(2); //switching to inmutable objects
+        assertEquals(new Dollar(10), product);
+
+        product = five.times(3); //switching to inmutable objects
+        assertEquals(new Dollar(15), product);
+    }
+
+    @Test
+    void testEqualityDollar() {
+        assertEquals(new Dollar(5), new Dollar(5));
+        assertNotEquals(new Dollar(5), new Dollar(8));
+        assertNotEquals(new Dollar(5), new Franc(5));
+    }
+
+    @Test
+    void testMultiplicationFranc() {
+        Franc five = new Franc(5);
+
+        Franc product = (Franc) five.times(2); //switching to inmutable objects
+        assertEquals(new Franc(10), product);
+
+        product = five.times(3); //switching to inmutable objects
+        assertEquals(new Franc(15), product);
+    }
+
+    @Test
+    void testEqualityFranc() {
+        assertEquals(new Franc(5), new Franc(5));
+        assertNotEquals(new Franc(5), new Franc(8));
+    }
+}
