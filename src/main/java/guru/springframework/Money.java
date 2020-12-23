233dcd6e12600daea3590284afd181dfe8766db3
Igor Fraga <igorf.ext@vr.com.br>
2020-12-22 22:08:26 -0300

    Exercices from Sector 3 - TDD by Example until Chapter 27, The Root of all Evil

--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -3,19 +3,40 @@ package guru.springframework;
 public class Money {
 
     protected int amount;
-//
-//    public Money(int amount) {
-//        this.amount = amount;
-//    }
-//
-//    Money times(int multiplier) {
-//        return new Money(amount * multiplier);
-//    }
-//
+    protected String currency;
+
+    public Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    protected String currency() {
+        return currency;
+    }
+
+    public static Money dollar(int amount) {
+        return new Money(amount, "USD");
+    }
+
+    public static Money franc(int amount) {
+        return new Money(amount, "CHF");
+    }
+
     public boolean equals(Object object) {
         Money money = (Money) object;
         return this.amount == money.amount
-                && this.getClass().equals(object.getClass()); //compares also the type of the Currency
+                && this.currency == money.currency; //compares also the type of the Currency
     }
 
+    @Override
+    public String toString() {
+        return "Money{" +
+                "amount=" + amount +
+                ", currency='" + currency + '\'' +
+                '}';
+    }
+
+    public Money times(int multiplier) {
+        return new Money(amount * multiplier, this.currency);
+    }
 }
--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -7,37 +7,29 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class MoneyTest {
     @Test
-    void testMultiplicationDollar() {
-        Dollar five = new Dollar(5);
+    void testMultiplication() {
+        Money five = Money.dollar(5);
+        assertEquals(Money.dollar(10), five.times(2));
+        assertEquals(Money.dollar(15), five.times(3));
 
-        Dollar product = five.times(2); //switching to inmutable objects
-        assertEquals(new Dollar(10), product);
-
-        product = five.times(3); //switching to inmutable objects
-        assertEquals(new Dollar(15), product);
+        five = Money.franc(5);
+        assertEquals(Money.franc(10), five.times(2));
+        assertEquals(Money.franc(15), five.times(3));
     }
 
     @Test
-    void testEqualityDollar() {
-        assertEquals(new Dollar(5), new Dollar(5));
-        assertNotEquals(new Dollar(5), new Dollar(8));
-        assertNotEquals(new Dollar(5), new Franc(5));
+    void testEquality() {
+        assertEquals(Money.dollar(5), Money.dollar(5));
+        assertNotEquals(Money.dollar(5), Money.dollar(8));
+        assertNotEquals(Money.dollar(5), Money.franc(5));
+
+        assertEquals(Money.franc(5), Money.franc(5));
+        assertNotEquals(Money.franc(5), Money.franc(8));
     }
 
     @Test
-    void testMultiplicationFranc() {
-        Franc five = new Franc(5);
-
-        Franc product = (Franc) five.times(2); //switching to inmutable objects
-        assertEquals(new Franc(10), product);
-
-        product = five.times(3); //switching to inmutable objects
-        assertEquals(new Franc(15), product);
-    }
-
-    @Test
-    void testEqualityFranc() {
-        assertEquals(new Franc(5), new Franc(5));
-        assertNotEquals(new Franc(5), new Franc(8));
+    void testCurrency() {
+        assertEquals("USD", Money.dollar(1).currency());
+        assertEquals("CHF", Money.franc(1).currency());
     }
 }
